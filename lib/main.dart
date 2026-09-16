61286139ee8632baeab9fcb03cc729bee255a4b5
yakubandrean <yakub.andrean@gmail.com>
2026-09-16 15:05:09 +0700

    Hanya refresh saja

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Tugas9(),
+      home: const Tugas9App(),
     );
   }
 }
